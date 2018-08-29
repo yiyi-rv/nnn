c0851b277db99960fcac0cd874ca12e6ad441aa2
Yiyi Wang <yiyi.wang@runtimeverification.com>
2018-08-29 21:18:20 +0000

    Initial commit

---
+++ rv.sh
@@ -0,0 +1,10 @@
+set -x
+
+json_out=`pwd`/errors.json
+report_out=`pwd`/report
+
+apt install -y libncursesw5-dev libreadline6-dev
+CC=kcc LD=kcc CFLAGS="-fissue-report=$json_out" make -j`nproc`
+
+rv-html-report $json_out -o $report_out
+# rv-upload-report $report_out
