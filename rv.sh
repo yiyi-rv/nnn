f4f8975ff5c27ac4aa0020310837d73328f77f02
yiyiwang <shd101wyy@sina.com>
2018-09-04 16:31:35 -0500

    Update rv.sh to run compiled program

--- rv.sh
+++ rv.sh
@@ -6,6 +6,7 @@ report_out=`pwd`/report
 
 apt install -y libncursesw5-dev libreadline6-dev pkg-config
 CC=kcc LD=kcc CFLAGS="-fissue-report=$json_out" make -j`nproc`
+timeout 5s ./nnn
 
 rv-html-report $json_out -o $report_out
 rv-upload-report $report_out
